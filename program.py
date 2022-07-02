d9b2715e10adfbb675cc28aab206280ec705d4db
MaciejKusik <kusik.maciej@gmail.com>
2022-07-02 13:06:38 +0000

    Poprawki - butelka dla Chin-Maciej

--- program.py
+++ program.py
@@ -1,5 +1,8 @@
 wiek = input("Podaj wiek użytkownika: ")
 #Sprawdzamy czy wiek jest złożony z cyfr
+region = input ("Jesli jestes z Chin, wpisz 0, jesli nie 1")
+if region.isdigit() == False:
+    exit("Podano nieprawidlowa")
 if wiek.isdigit() == False:
     exit("Wiek mosi być podany jako liczba")
 wiek = int(wiek)
@@ -10,3 +13,12 @@ elif wiek > 50:
     print("W Twoim wieku alkohol jest już szkodliwy")
 else:
     exit("Jesteś za młody na alkohol")
+if region.isdigit() == False:
+    exit("Podano nieprawidlowa wartosc")
+region = int(region)
+if region == 0:
+    print("Uzytkownicy z Chin dostaja darmowa butelke whiskey")
+elif region == 1:
+    print("Uzytkownicy z poza Chin nie dostaja darmowej butelki whiskey")
+else:
+    exit("Podaj 0 lub 1")
