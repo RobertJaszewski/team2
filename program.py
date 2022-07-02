0616d03feb0fad56c612cefab7321f5059e780aa
ppelka6 <ppelka6@user.noreply.github.com>
2022-07-02 14:53:40 +0200

    Added new option

--- program.py
+++ program.py
@@ -10,3 +10,18 @@ elif wiek > 50:
     print("W Twoim wieku alkohol jest już szkodliwy")
 else:
     exit("Jesteś za młody na alkohol")
+
+**********************************************
+
+płeć = input("Jaka jest Twoja plec?")
+wiek = input("Podaj swoj wiek: ")
+#Sprawdzamy płeć oraz wiek użytkownika
+wiek = int(wiek)
+płeć = int(plec)
+płeć = input("Podaj płeć użytkownika: \n 'k'-kobieta \n 'm'-mężczyzna \n")
+if wiek >= 18:
+    print(Specjalna usługa dla kobiet pełnoletnich!)
+if płeć = 'k':
+    print(Dostajesz od nas APEROLA!)
+else:
+    exist("Jesteś pełnoletni ale nie jesteś kobietą")
