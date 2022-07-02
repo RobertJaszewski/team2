3c3cd8dd2ed73c132aae0e935b813a32d4508005
AdamOsses <adamosses@users.noreply.github.com>
2022-07-02 15:11:11 +0200

    Updated program.py USA/EU

--- program.py
+++ program.py
@@ -1,3 +1,23 @@
+# Adam61398 - Wybor regionu: USA/Europa
+minimalny_wiek = 0
+print(40*'*')
+print('WITAJ NA STRONIE SKLEPU "MALY ALHOHOLIK"\n')
+while True:
+    print('Wybierz region:\n\t1. USA\n\t2. Europa')
+    region = str(input())
+    if region.lower() == 'USA' or region == '1' or region == '1.':
+        print('Wybrany region: USA (dorosly od 21 lat)')
+        minimalny_wiek = 21
+        break
+    if region.lower() == 'Europa' or region == '2' or region == '2.':
+        print('Wybrany region: Europa (dorosly od 18 lat)')
+        minimalny_wiek = 21
+        break
+    print('\nBledny region.')
+print(40*'*')
+# Adam61398 - Koniec: wybor regionu: USA/Europa
+# Zmienna "minimalny_wiek" podaje minimalny wiek od ktorego mozna nabyc alkohol
+
 wiek = input("Podaj wiek użytkownika: ")
 #Sprawdzamy czy wiek jest złożony z cyfr
 if wiek.isdigit() == False:
